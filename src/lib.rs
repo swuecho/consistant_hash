30a1ea5fa4359152390d63670cf6bf17c7644f5b
Hao Wu <hwu@intellisurvey.com>
2015-04-30 01:11:22 -0400

    get the type right

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@ use crc::crc32;
 use std::cmp::Ordering;
 
 struct Node {
-    id : String,
+    id : &'static str,
     hash_key: u32,
 }
 
 impl Node {
 
-fn new(id: String) -> Node {
+fn new(id: &'static str ) -> Node {
     Node { id: id, hash_key: crc32::checksum_ieee(id.as_bytes()) }
 }
 
@@ -20,14 +20,15 @@ impl Eq for Node {}
 
 impl PartialEq for Node {
     fn eq(&self, other: &Node) -> bool {
-      self.id.eq(&other.id)
+        // why eq is not right
+      self.id == other.id
     }
 }
 
 impl Ord for Node {
 
     fn cmp(&self, other: &Node) -> Ordering {
-      self.id.cmp(&other.id)
+      self.id.cmp(other.id)
     }
 
 }
@@ -35,9 +36,10 @@ impl Ord for Node {
 impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
         // precedence
-      self.id.partial_cmp(&other.id)
+      self.id.partial_cmp(other.id)
     }
 }
+
 
 
 type Nodes = Vec<Node>;
@@ -56,10 +58,27 @@ fn new () -> Ring {
 }
 
 
-fn add_node(&mut self, id : String) {
+fn add_node(&mut self, id : &'static str) {
       let node = Node::new(id);
       self.nodes.push(node);
       self.nodes.sort(); 
+}
+
+fn get(&self, key: &'static str) -> &str {
+     let hash = crc32::checksum_ieee(key.as_bytes());
+     //
+     let result = self.nodes.binary_search_by( |node | node.hash_key.cmp(&hash) );
+
+     let mut index = match result {
+        Ok(i)  => i,
+        Err(j) => j,
+     };
+
+     if index == self.nodes.len() {
+        index = 0;
+     }
+
+     self.nodes[index].id
 }
 
 }
